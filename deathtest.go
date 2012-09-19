5b6c8b917a92c480ac3ca3685676fb37c034aed9
Peter Waller <peter.waller@cern.ch>
2012-09-19 13:45:16 +0100

    go fmt

--- deathtest.go
+++ deathtest.go
@@ -1,12 +1,12 @@
 package deathtest
 
 import (
-    "flag"
-    "log"
-    "os/exec"
-    "reflect"
-    "strings"
-    "testing"
+	"flag"
+	"log"
+	"os/exec"
+	"reflect"
+	"strings"
+	"testing"
 )
 
 var deathtest_running = flag.Bool("deathtest.running", false, "death test is running")
@@ -14,26 +14,28 @@ var deathtest_running = flag.Bool("deathtest.running", false, "death test is run
 type deathtestWriter struct{}
 
 func (m deathtestWriter) Write(in []byte) (int, error) {
-    for _, l := range strings.Split(string(in), "\n") {
-        log.Print("  @DT: ", l)
-    }
-    return len(in), nil
+	for _, l := range strings.Split(string(in), "\n") {
+		log.Print("  @DT: ", l)
+	}
+	return len(in), nil
 }
 
 func Run(t *testing.T) bool {
-    if *deathtest_running { return true }
-    name := reflect.ValueOf(*t).FieldByName("name").String()
-    
-    args := []string{"go", "test", "-deathtest.running", "-run", name}
-    log.Printf("deathtest.Run(%v)", args)
-    p := exec.Command(args[0], args[1:]...)
-    w := deathtestWriter{}
-    p.Stdout, p.Stderr = w, w
-    result := p.Run()
-    
-    if result == nil {
-        log.Print("deathtest.Run: Failure to fail")
-        t.Fail()
-    }
-    return false
+	if *deathtest_running {
+		return true
+	}
+	name := reflect.ValueOf(*t).FieldByName("name").String()
+
+	args := []string{"go", "test", "-deathtest.running", "-run", name}
+	log.Printf("deathtest.Run(%v)", args)
+	p := exec.Command(args[0], args[1:]...)
+	w := deathtestWriter{}
+	p.Stdout, p.Stderr = w, w
+	result := p.Run()
+
+	if result == nil {
+		log.Print("deathtest.Run: Failure to fail")
+		t.Fail()
+	}
+	return false
 }
